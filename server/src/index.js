230e190680396b461efdbff4109cf53c65806f42
Badri-Vishal <badrivishalpadhy@gmail.com>
2024-12-09 20:10:58 +0530

    step2:Appllo-server

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,16 @@
-//TODO
+const { ApolloServer } = require("@apollo/server");
+const { startStandaloneServer } = require("@apollo/server/standalone");
+const typeDefs = require("./schema");
+
+
+async function startApolloServer( ) {
+  const server  =  new ApolloServer({ typeDefs });
+   const { url }  = startStandaloneServer(server)
+    console.log(`
+      Server is running!
+      Query at ${url}
+     
+      `)
+  }
+
+  startApolloServer();
