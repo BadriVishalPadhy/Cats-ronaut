8dafe2350d7fb56f9b5fe8a83791ae44f845434e
Badri-Vishal <badrivishalpadhy@gmail.com>
2024-12-09 19:47:45 +0530

    step1:building schema

---
+++ client/src/schema.js
@@ -0,0 +1,36 @@
+const gql  = require("graphql_tag")
+
+const typeDefs  =  gql`
+ " We define Query type because we dont have end points like in rest api "
+ "Query type are Entry points to the rest of the Schema "
+ "Query to get Tracks array for the homepage grid "
+  "Fetching the track list for the homepage"
+
+
+
+ type Query {
+  
+  tracksForHome: [Track!]!
+ }  
+
+
+
+type Track {
+  id: ID!
+  title: String!
+  author: Author!
+  thumbnail:String
+  length: Int
+  modulesCount: Int
+
+
+} 
+
+ type Author {
+  id: ID!
+  name: String!
+  photo:String
+ }
+
+
+`
