d71657823be6e51ed858df53d174cf6e6eebd150
Badri-Vishal <badrivishalpadhy@gmail.com>
2024-12-09 22:12:14 +0530

    step4:FE Defining a query

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import { Layout } from '../components';
+import {gql} from "@apollo/client"
 
-/**
- * Tracks Page is the Catstronauts home page.
- * We display a grid of tracks fetched with useQuery with the TRACKS query
- */
+const TRACKS = gql`
+
+query TracksForHome {
+  tracksForHome {
+    title
+    thumbnail
+    modulesCount
+    length
+    id
+    author {
+      photo
+      name
+      id
+    }
+  }
+}
+
+`
+
 const Tracks = () => {
   return <Layout grid> </Layout>;
 };
